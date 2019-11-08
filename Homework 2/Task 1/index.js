4bfb4dbd844c0d8035f8dc965805126c138ea9f8
YuryKarlianok <yury.karlianok@leverx.com>
2019-11-08 08:09:03 -0800

    Commit 2.0.0 (Task 1 & Task2)

---
+++ Homework 2/Task 1/index.js
@@ -0,0 +1,8 @@
+function google() {
+    window.open("https://www.google.com/");
+}
+
+function clearall() {
+    document.getElementById("name").innerHTML = "";
+    document.getElementById("revname").innerHTML = "";
+}
---
+++ Homework 2/Task 2/index.js
@@ -0,0 +1,41 @@
+window.onload = function() {
+    var username = prompt("Please enter your name");
+    document.getElementById("name").innerHTML = username;
+    if (hasNumber(username) == true) {
+        document.getElementById("revname").innerHTML = reversename(username);
+    }
+    else {
+        document.getElementById("revname").innerHTML = upperlowername(username);
+    }
+};
+
+function google() {
+    window.open("https://www.google.com/");
+}
+
+function clearall() {
+    document.getElementById("name").innerHTML = "";
+    document.getElementById("revname").innerHTML = "";
+}
+
+function hasNumber(str) {
+    return /\d/.test(str);
+};
+
+function reversename(str) {
+    return str.split("").reverse().join("");
+}
+
+function upperlowername(str) {
+    str = str.split("");
+    for (var i = 0; i < str.length; i++) {
+        if (i % 2 == 0) {
+            str[i] = str[i].toUpperCase();
+        }
+        else {
+            str[i] = str[i].toLowerCase();
+        }
+    }
+    str = str.join("");
+    return str;
+}
